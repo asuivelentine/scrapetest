10723d167cc5394bd5fbe77240b3d629966ff2cd
Kai Sickeler <k.sickeler@gmail.com>
2017-07-22 22:11:20 +0200

    get_named final

--- src/main.rs
+++ src/main.rs
@@ -4,35 +4,40 @@ extern crate regex;
 
 use select::document::Document;
 use select::predicate::{Attr, Name, And};
+use regex::RegexBuilder;
 use regex::Regex;
 
 
-fn run() -> Result<(), ()> {
+fn get_current_dax() -> Vec<String> {
     let res = reqwest::get("http://www.boerse-online.de/index/liste/DAX")
-        .map_err(|e| ())
+        .map_err(|_| ())
         .and_then(|r| Document::from_read(r)
-            .map_err(|e| ()));
+            .map_err(|_| ()));
 
     if let Ok(d) = res {
         let pat  = Regex::new(r"/aktie/(.*)-Aktie").unwrap();
+        let pat = RegexBuilder::new(r"/aktie/(.*)-Aktie")
+            .multi_line(true)
+            .build()
+            .unwrap();
+
         let predicate = And(Name("a"), And(Attr("href", ()), Attr("title", ()))); 
         let x = d.find(predicate)
             .map(|d| d.html())
             .map(|h| pat.captures(&h)
-                 .map(|c| c[0].to_string()))
+                 .map(|c| c[1].to_string()))
             .filter(|s| s.is_some())
             .map(|s| s.unwrap())
             .collect::<Vec<String>>();
-
         
-        for w in x {
-            println!("{:?}", w);
-        }
+        return x;
     }
-    Ok(())
+    Vec::new()
 }
 
 
 fn main() {
-    run();
+    for c in get_current_dax() {
+        println!("{}", c);
+    }
 }
